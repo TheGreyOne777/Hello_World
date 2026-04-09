82cec638b5ddc21d5886d428524f4872cf0441f6
Armand <armand199701@gmail.com>
2026-04-09 10:22:27 +0200

    Odd numbers and EVEN numbers

---
+++ Odd_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            Console.Write("Insert any number: ");
+            x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.Write("This is an Even number: ");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("This is an ODD number ");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ sum_two_numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Insert second number: ");
+            int num2= int.Parse(Console.ReadLine());
+            int sum = num1 + num2;
+
+            Console.WriteLine("The sum of: " + num1 + " + " + num2 + " is: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
