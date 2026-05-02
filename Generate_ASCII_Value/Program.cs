cb9c52bde31b71729bdc982f11bd9983d0dd7152
Armand <armand199701@gmail.com>
2026-05-02 11:22:08 +0200

    Adding ASCII value code

---
+++ Generate_ASCII_Value/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generate_ASCII_Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+
+            Console.Write("Insert any Characcter: ");
+            ch = Char.Parse(Console.ReadLine());
+
+            Console.WriteLine("The value of the ASCII character is: " + (int)ch);
+
+            Console.ReadLine();
+        }
+    }
+}
