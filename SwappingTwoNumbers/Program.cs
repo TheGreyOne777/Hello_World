4ce49a5ef7cfe842af8f328db0ca18a3335da3fc
Armand <armand199701@gmail.com>
2026-05-02 12:04:59 +0200

    Leapr Year

---
+++ SwappingTwoNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwappingTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, xswap;
+
+            Console.Write("Insert first number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Insert second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numbers before swapping are: {0} and {1}", num1, num2);
+
+            xswap = num1;
+            num1 = num2;
+            num2 = xswap;
+
+            Console.WriteLine("Numbers after swapping are: {0} and {1}", num1, num2);
+
+            Console.ReadKey();
+        }
+    }
+}
